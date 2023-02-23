e3ec35db800f4dd8a5833ce7e939d4140156920d
Sagar <72916095+sagarandroiddeveloper@users.noreply.github.com>
2023-02-23 17:31:31 +0530

    latest 2

---
+++ app/src/main/java/com/app/demo/bestnetworkapp/CommanClass/Parameter_Class.java
@@ -0,0 +1,33 @@
+package com.app.demo.bestnetworkapp.CommanClass;
+
+import com.app.demo.bestnetworkapp.Model.CountryList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parameter_Class {
+
+    public static boolean server_Start = false;
+
+    public static ArrayList<CountryList> countryLists =  new ArrayList<>();
+    public static Boolean Server_random= false;
+    public static Boolean Server_Show= true;
+    public static Boolean Server_Direct_Connect= true;
+    public static String Privacy_policy= "";
+
+    // TODO :  ID PASS
+    public static String Server_Id = "touchvpn";
+    public static String Server_password = "Nopassword";
+
+    // TODO : URL
+    public static Boolean url_Type= false;
+    public static String Server_Url_Default = "https://backend.northghost.com";
+    public static List<String> unknown_url_list = new ArrayList<>();
+
+
+    // TODO : DEFAULT COUNTRY CONNECT
+    public static String Server_code = "US";
+    public static String Server_name = "United States";
+    public static String Server_imageurl = "http://157.245.125.183:1161/upload/countryimg-1653906499006.png";
+
+}
--- app/src/main/java/com/app/demo/bestnetworkapp/MainActivity.java
+++ app/src/main/java/com/app/demo/bestnetworkapp/MainActivity.java
@@ -1,10 +1,14 @@
 package com.app.demo.bestnetworkapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import com.app.demo.bestnetworkapp.CommanClass.Parameter_Class;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +19,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public static void Toast_Sp(Context context){
-        Toast.makeText(context, "Welcome To Our App ", Toast.LENGTH_SHORT).show();
+    public static OnConnect onConnect;
+
+    public static interface OnConnect {
+        void onconnect(String connect);
+    }
+
+    public static Activity contexts;
+
+    public static void Toast_Sp(Activity context, OnConnect onConnect_new) {
+        contexts = context;
+        onConnect = onConnect_new;
+        Toast.makeText(context, "Welcome To Our Apps", Toast.LENGTH_SHORT).show();
+        onConnect.onconnect("OK");
+    }
+
+
+    public static void stop(OnConnect onConnect_new) {
+        onConnect = onConnect_new;
+        onConnect.onconnect("stop");
+    }
+
+    public static void start() {
+        onConnect.onconnect("stop");
     }
 }
